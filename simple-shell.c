4d17c06d05a07e208d8b17cad4588ea3b5dede4f
Ben Swaffield <29522680+BenSwaffield@users.noreply.github.com>
2024-01-19 17:00:05 +0000

    Comments

--- simple-shell.c
+++ simple-shell.c
@@ -6,55 +6,70 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+/*
+ *Forks and executes an external program.
+ *@param command The external process to run.
+ *@param arguments An array of arguments for the external process. Note that the first element of this array IS NOT the command, unlike the array passed to the execvp function.
+ *@param args_len The amount of arguments for the external process.
+ *@return 0 is successful, error code if not.
+ */
 int forky_fun(char *command, char* arguments[], int args_len){
-    pid_t parent = getpid();
     pid_t pid = fork();
     if (pid == -1) {
+        perror("Failed to create process");
         return -1;
     } else if (pid > 0) {
         int status;
         waitpid(pid, &status, 0); // Wait for the child to finish
         if (status == 0){
             return 0;
-        } else if (status==256) {
-            printf("Error, command not found\n");
+        } else if (status==256) { // Program not found error code
+            printf("Error: Command not found\n");
             return -1;
-        } 
-        else {
+        } else if (status==512){ // Argument not found
+            return -1;
+        } else {
             printf("Status:%d\n", status);
             return -1;
         }
         
     } else { // This is the child proccess
-        char **argv; // Pointer for the argument array
+        // Create pointer for the argument array and allocate memory
+        char **argv;
         argv = malloc(sizeof(char *) * args_len+2);
-    
+        // Put the command in the first position of the argument array, as required by execvp
         argv[0] = malloc(strlen(command));
         strcpy(argv[0], command);
-
+        // Copy all the arguments into the array
         int i = 0;
         while (i<args_len){
             argv[i+1] = malloc(strlen(arguments[i]));
             strcpy(argv[i+1], arguments[i]);
             i++;
         }
+        // Finish the array with NULL, as required by execvp
         argv[i+1] = malloc(sizeof(NULL));
         argv[i+1] = NULL;
+        // Execute
         execvp(command, argv);
         _exit(EXIT_FAILURE);
     }
 }
 
+/*
+ *@param str The string to tokenize.
+ *@param tokens A double pointer for tokenize to store the formated tokens.
+ *@return The number of tokens.
+ */
 int tokenize(char *str, char **tokens){
-    /*
-    Return the number of tokens.
-    Tokens are put in tokens.
-    */
+    
     char *token; 
     int i = 0;
     token = strtok(str, DELIMITERS);
     while (token != NULL){
+        // Allocate memory to store the token
         tokens[i] = malloc(strlen(token));
+        // Copy the token
         strcpy(tokens[i], token);
         token = strtok(NULL, DELIMITERS);
         i++;
